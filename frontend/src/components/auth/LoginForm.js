31c202667c83cea60f8c171e8f4d5f7484ccbdbd
Rikie Patrick <rikiepatrick@gmail.com>
2023-09-19 14:31:35 +0100

    styles updates

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const LogInForm = ({ navigate }) => {
   const [email, setEmail] = useState("");
@@ -7,41 +7,75 @@ const LogInForm = ({ navigate }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    let response = await fetch( '/tokens', {
-      method: 'post',
+    let response = await fetch("/tokens", {
+      method: "post",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email: email, password: password })
-    })
+      body: JSON.stringify({ email: email, password: password }),
+    });
 
-    if(response.status !== 201) {
-      console.log("yay")
-      navigate('/login')
+    if (response.status !== 201) {
+      console.log("yay");
+      navigate("/login");
     } else {
-      console.log("oop")
-      let data = await response.json()
-      window.localStorage.setItem("token", data.token)
-      navigate('/posts');
+      console.log("oop");
+      let data = await response.json();
+      window.localStorage.setItem("token", data.token);
+      navigate("/posts");
     }
-  }
+  };
 
   const handleEmailChange = (event) => {
-    setEmail(event.target.value)
-  }
+    setEmail(event.target.value);
+  };
 
   const handlePasswordChange = (event) => {
-    setPassword(event.target.value)
-  }
+    setPassword(event.target.value);
+  };
 
-
-    return (
-      <form onSubmit={handleSubmit}>
-        <input placeholder='Email' id="email" type='text' value={ email } onChange={handleEmailChange} />
-        <input placeholder='Password' id="password" type='password' value={ password } onChange={handlePasswordChange} />
-        <button type="submit" id='submit' className="btn btn-primary">Login</button>
+  return (
+    <div className="min-vh-100 d-flex align-items-center">
+      <form
+        className="container col-3 login-form rounded-4 p-3"
+        onSubmit={handleSubmit}
+      >
+        <div className="mb-3">
+          <h1 className="text-center">Login</h1>
+          <label for="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Email"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="Password"
+            className="form-control"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <div className="d-flex justify-content-center">
+          <button type="submit" id="submit" className="btn btn-primary w-50">
+            Login
+          </button>
+        </div>
       </form>
-    );
-}
+    </div>
+  );
+};
 
 export default LogInForm;
