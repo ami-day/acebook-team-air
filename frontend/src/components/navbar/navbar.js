9342b1b8ce007f3eae674b72e0d59c43a2dc077a
Tom Whelan <thomas.whelan20@icloud.com>
2023-09-20 13:53:52 +0100

    added username style

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,26 +1,19 @@
 import React from "react";
+import './navbar.css';
 
 const Navbar = ({ onLogout }) => {
-  const navbarStyle = {
-    position: "fixed",
-    top: "0",
-    left: "0",
-    width: "100%",
-    backgroundColor: "aliceblue",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: "10px",
-    zIndex: "100",
-  };
+
 
   return (
     <div>
-      <nav className="nav" id="navbar" style={navbarStyle}>
+      <nav className="nav" id="navbar">
         <h2>AceBook</h2>
+        <div className="logout">
+        <h5>username</h5>
         <button className="btn btn-danger" onClick={onLogout}>
           Logout
         </button>
+        </div>
       </nav>
     </div>
   );
