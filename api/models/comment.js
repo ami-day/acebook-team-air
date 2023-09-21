305132a956e794b89046bd135cb72a678448976c
Rikie Patrick <rikiepatrick@gmail.com>
2023-09-21 23:28:43 +0100

    timestamps added to comments

--- api/models/comment.js
+++ api/models/comment.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const CommentSchema = new mongoose.Schema({
-  content: { type: String, required: true },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+const CommentSchema = new mongoose.Schema(
+  {
+    content: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Comment = mongoose.model("Comment", CommentSchema);
 
