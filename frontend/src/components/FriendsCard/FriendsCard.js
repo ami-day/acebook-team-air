3f688ddc390023cd5dcab6d1c191ff1f3558fed0
Ami Day <amiday@Amis-MacBook-Pro-2.local>
2023-09-25 12:00:44 +0100

    Started adding find friends modal

--- frontend/src/components/FriendsCard/FriendsCard.js
+++ frontend/src/components/FriendsCard/FriendsCard.js
@@ -1,14 +1,39 @@
 import "./FriendsCard.css";
-import React from "react";
-import FriendProfile from "./FriendProfile"
+import React, { useEffect, useState } from "react";
+import FriendProfile from "./FriendProfile";
+import FriendsModal from "../feed/FriendsModal";
 
 const FriendsCard = () => {
+
+    const [modal,setModal] = useState(false);
+
+    const showModal = () => {
+        setModal(true);
+    }
+
+    if (modal) {
+        return (
+            <div id="friends-card">
+            <div id="card-btns">
+                <button className="btn btn-primary">Friends</button>
+                <button className="btn btn-primary" onClick={showModal}>Find Friends</button>
+            </div>
+            <FriendsModal></FriendsModal>
+            <div id="friend-profiles">
+                <FriendProfile></FriendProfile>
+                <FriendProfile></FriendProfile>
+                <FriendProfile></FriendProfile>
+                <FriendProfile></FriendProfile>
+            </div>
+        </div>
+        )
+    } else {
 
     return (
         <div id="friends-card">
             <div id="card-btns">
                 <button className="btn btn-primary">Friends</button>
-                <button className="btn btn-primary">Messages</button>
+                <button className="btn btn-primary" onClick={showModal}>Find Friends</button>
             </div>
             <div id="friend-profiles">
                 <FriendProfile></FriendProfile>
@@ -18,6 +43,6 @@ const FriendsCard = () => {
             </div>
         </div>
     )
-
     }
+}
 export default FriendsCard;
---
+++ frontend/src/components/feed/FriendsModal.js
@@ -0,0 +1,18 @@
+import './FriendsModal.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+const FriendsModal = () => {
+
+return (
+  <div id="find-friends">
+<h1>Find Friends</h1>
+<p>This is where you can find friends!</p>
+<div class="form">
+  <i class="fa fa-search"></i>
+  <input type="text" class="form-control form-input" placeholder="Search friends..."/>
+  </div>
+</div>        
+)
+}
+export default FriendsModal;
+
